e588591f06b2deea4e4c31344986b8917e3706b1
roberto2002-03-12 <emilio.contreras@tecsup.edu.pe>
2023-05-23 10:17:13 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const db = require('./db');
+
+const router = require('./network/routes');
+
+db(process.env.URL_DB);
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+router(app);
+
+app.use('/app', express.static('public'));
+
+app.listen(3000);
+console.log('La aplicación está escuchando en http://localhost:3000');
